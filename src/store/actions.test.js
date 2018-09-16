3da3c9329b537e81c9de9da7ca9fcf7eaf30598b
Kasia Zadurska <kzadurska@gmail.com>
2018-09-16 17:59:10 +0200

    Rename variable

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -3,14 +3,13 @@ import configureStore from 'redux-mock-store'
 import thunkMiddleware from 'redux-thunk'
 import reducer from './reducers'
 import { readSettings as apiReadSettings } from './api';
-import { readSettings } from './actions'
+import { readSettings as actionReadSettings } from './actions'
 import * as TYPES from './constants';
-
 
 describe('action', () => {
   let store;
   const mockStore = configureStore([thunkMiddleware])
-  
+
   beforeEach(() => {
     store = mockStore({})
     store.replaceReducer(reducer)
@@ -22,10 +21,9 @@ describe('action', () => {
 
   test('readSettings', async () => {
     expect(store.getActions()).toEqual([])
-    
     expect(apiReadSettings).not.toHaveBeenCalled();
 
-    await store.dispatch(readSettings())
+    await store.dispatch(actionReadSettings())
       .then(() => {
         expect(store.getActions()).toEqual([
           { type: TYPES.READ_SETTINGS_START },
